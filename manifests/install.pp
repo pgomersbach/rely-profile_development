63f60832d79cbe7f56b958576759941649f32181
P. Gomersbach <p.gomersbach@rely.nl>
2017-09-24 22:03:09 +0000

    add kubctl

--- manifests/install.pp
+++ manifests/install.pp
@@ -36,4 +36,11 @@ class profile_development::install {
     source => 'https://github.com/kubernetes/kops/releases/download/1.7.0/kops-linux-amd64',
   }
   # install kubctl
+  file { 'kubctl':
+    ensure => 'file',
+    path   => '/usr/local/bin/kubctl',
+    mode   => 'a+x',
+    source => 'https://storage.googleapis.com/kubernetes-release/release/v1.7.6/bin/linux/amd64/kubectl',
+  }
+
 }
