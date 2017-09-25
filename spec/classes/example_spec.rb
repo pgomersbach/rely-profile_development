8a4ce77557dc96a67e2d979ed1b1a2442a00ff08
P. Gomersbach <p.gomersbach@rely.nl>
2017-09-25 13:23:02 +0000

    add spec tests

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -20,6 +20,32 @@ describe 'profile_development' do
           it { is_expected.to contain_class('profile_development::config') }
           it { is_expected.to contain_class('profile_development::service') }
 
+          it { is_expected.to contain_file('kops_flags') }
+          it { is_expected.to contain_file('kubctl_flags') }
+          it { is_expected.to contain_remote_file('kops') }
+          it { is_expected.to contain_remote_file('kubctl') }
+
+          case facts[:osfamily]
+          when 'Debian'
+            it { is_expected.to contain_apt__source('azurecli') }
+            it { is_expected.to contain_apt__ppa('ppa:ansible/ansible') }
+            it { is_expected.to contain_apt__ppa('ppa:brightbox/ruby-ng') }
+          end
+          case facts[:operatingsystem]
+          when 'ubuntu'
+            it { is_expected.to contain_package('ruby2.3') }
+            it { is_expected.to contain_package('ruby2.3-dev') }
+            it { is_expected.to contain_package('git') }
+            it { is_expected.to contain_package('bundler') }
+            it { is_expected.to contain_package('unzip') }
+            it { is_expected.to contain_package('awscli') }
+            it { is_expected.to contain_package('python-novaclient') }
+            it { is_expected.to contain_package('python-neutronclient') }
+            it { is_expected.to contain_package('python-glanceclient') }
+            it { is_expected.to contain_package('software-properties-common') }
+            it { is_expected.to contain_package('ansible') }
+            it { is_expected.to contain_package('azure-cli') }
+          end
         end
       end
     end
