245a7c1e6479ca678f98bd867f51b4409782bada
P. Gomersbach <p.gomersbach@rely.nl>
2017-09-25 18:44:10 +0000

    add onlyif

--- manifests/githuborg.pp
+++ manifests/githuborg.pp
@@ -5,5 +5,6 @@ define profile_development::githuborg (
   exec { $orgname:
     command => "wget --no-check-certificate --quiet -qO- https://api.github.com/orgs/${orgname}/repos | jq '.[].clone_url' | xargs -L 1 git clone",
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    unless  => "wget --no-check-certificate --quiet -qO- https://api.github.com/orgs/${orgname}/repos | jq '.[].clone_url' | cut -f5 -d '/'|cut -f1 -d'.'|head -1| xargs ls",
   }
 }
