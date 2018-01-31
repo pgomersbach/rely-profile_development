0128ca8c229f5d9ce95b3b233f28787a4899e390
P. Gomersbach <p.gomersbach@rely.nl>
2018-01-31 11:52:43 +0000

    add python-mysql.connector package

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,7 +11,7 @@ class profile_development::params {
     }
     'Ubuntu': {
       $devuser = 'ubuntu'
-      $packages = ['ruby2.3', 'ruby2.3-dev', 'git' ,'bundler', 'unzip', 'python-pip', 'python-novaclient', 'python-neutronclient', 'python-glanceclient', 'software-properties-common', 'ansible', 'azure-cli', 'jq', 'google-cloud-sdk', 'xvfb', 'gconf-service', 'libasound2', 'libatk1.0-0', 'libcairo2', 'libcups2', 'libfontconfig1', 'libgconf-2-4', 'libgdk-pixbuf2.0-0', 'libgtk-3-0', 'libnspr4', 'libxss1', 'fonts-liberation', 'libappindicator1', 'libnss3', 'xdg-utils' ]
+      $packages = ['ruby2.3', 'ruby2.3-dev', 'git' ,'bundler', 'unzip', 'python-pip', 'python-novaclient', 'python-neutronclient', 'python-glanceclient', 'software-properties-common', 'ansible', 'azure-cli', 'jq', 'google-cloud-sdk', 'xvfb', 'gconf-service', 'libasound2', 'libatk1.0-0', 'libcairo2', 'libcups2', 'libfontconfig1', 'libgconf-2-4', 'libgdk-pixbuf2.0-0', 'libgtk-3-0', 'libnspr4', 'libxss1', 'fonts-liberation', 'libappindicator1', 'libnss3', 'xdg-utils', 'python-mysql.connector' ]
     }
     default: {
       fail("${::operatingsystem} not supported")
